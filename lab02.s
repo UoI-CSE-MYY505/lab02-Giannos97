750426e215d2bc1a17562c022cbcd0d7c427fd3a
Giannos97 <cs05325@uoi.gr>
2024-10-21 20:54:04 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -13,6 +13,20 @@ prog:
 # Write your code here!
 # Do not remove the prog label or write code above it!
 #-----------------------------
-done:
+indLast_forwards_withIndex:
+    add  s0, zero, zero  # return address
+    add  t2, zero, zero  # index
+loop2:
+    beq  a1, t2, done2
+    slli t1, t2, 2
+    add  t1, t1, a0
+    lw   t3, 0(t1)
+    bne  t3, a2, next2
+    add  s0, t1, zero  # keep matching element's address in s0
+next2:
+    addi t2, t2, 1
+    j    loop2
+done2:
     addi a7, zero, 10 
     ecall
+
